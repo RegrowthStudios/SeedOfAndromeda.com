7f1e99b1942d8f30324fbcb9cb8a7cdf05147d29
SamThePsychoticLeprechaun <matthew.marshall96@yahoo.co.uk>
2014-03-16 22:42:23 +0000

    Introduced locking mechanism to slider...
    Locks the controls so that when pressed once, the user cannot press the
    next or previous buttons again for 0.5s.

--- scripts/pages/Screenshots.js
+++ scripts/pages/Screenshots.js
@@ -1,6 +1,7 @@
 ﻿$(document).ready(function () {
     var elem, index;
     var elems = $('.image');
+    var ctrlsLocked = false;
 
     elem = $(elems[0]);
     var thumbpath = $(elems[0]).attr('src');
@@ -23,7 +24,7 @@
 				.substring(0, fullimg.indexOf('_thumb_')) + fullimg
 				.substring(fullimg.length - 4)));
         $('html, body').animate({
-            scrollTop: $("#image-frame-inner").offset().top - 100
+            scrollTop: $("#image-frame-inner").offset().top - 150
         }, 1000);
         $('.enlarged-image').fadeOut(0, function () {
             $('.enlarged-image').attr('src', fullimg).fadeIn(0);
@@ -35,6 +36,7 @@
     });
 
     $('.img-next').click(function () {
+    	if(ctrlsLocked) { return; }
         var ind = elems.index(elem);
         if (ind == (elems.length - 1)) {
             elem = $(elems.get(0));
@@ -66,13 +68,14 @@
         
         index = elems.index(elem);
         preload(index - 1);
-        preload(index + 1);        
+        preload(index + 1);     
+        lockCtrls();
     });
 
 
     $('.img-prev').click(function () {
+    	if(ctrlsLocked) { return; }
         var ind = elems.index(elem);
-
         if (ind == 0) {
             elem = $(elems.get(elems.length - 1));
             index = elems.index(elem);
@@ -104,12 +107,20 @@
         index = elems.index(elem);
         preload(index - 1);
         preload(index + 1);
+        lockCtrls();
     });
     
     function preload(imgIndex) {
     	    	var image = $(elems.get(imgIndex));
     	    	var src = image.attr('src').replace('_thumb_202x162', '_thumb_700x480');
     	    	(new Image()).src = src;
+    }
+    
+    function lockCtrls() {
+    	ctrlsLocked = true;
+    	setTimeout(function() {
+    		ctrlsLocked = false;
+    	}, 500);
     }
 
 });

7c59e9fe8aa9a6235c7be144e7620e59601c9f8a
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2014-06-13 19:49:56 +0100

    Fixed Blog Header Colour

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <link rel="shortcut icon" type="image/x-icon"
 	href="/Assets/images/favicon.ico" />
 
-<link rel="stylesheet" href="/styles/soa.css?ver=6" type="text/css" />
+<link rel="stylesheet" href="/styles/soa.css?ver=7" type="text/css" />
 
 <link rel="stylesheet" href="/styles/Normalise.css" type="text/css" />
 

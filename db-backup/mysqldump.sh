947ff44194029e534b2d25eb4e0b541e82a2b24a
jessenic <jessenic@digiex.net>
2014-06-14 12:55:21 +0300

    Clean up old stuff from the repo

--- db-backup/mysqldump.sh
+++
@@ -1,3 +0,0 @@
-mysqldump -u root -ptest --all-databases | gzip > "../db-backup/backup/backup-$(date).sql.gz" 2> ../db-backup/dump.log
-
- echo "Finished mysqldump $(date)" >> ../db-backup/dump.log

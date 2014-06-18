07f8ee7a91c7803eee11a04b18e61a0bfed770ce
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2014-06-18 23:41:42 +0100

    Hide trailing text from Dev News

---
+++ scripts/pages/Home.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    var children = $("#blog-post-body > *");
+    var blogPostMaxHeight = parseInt($("#blog-post-body").css('maxHeight'), 10);
+    var heightCount = 0;
+    children.each(function () {
+        var height = parseInt($(this).height(), 10);
+        var marginBottom = parseInt($(this).css('marginBottom'), 10);
+        var totalHeight = height + marginBottom;
+        heightCount += totalHeight;
+
+        if (heightCount >= blogPostMaxHeight) {
+            $(this).hide();
+        }
+    });
+});

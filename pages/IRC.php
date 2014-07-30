d7ac2f4d4921f3a5bf3db3729387327d9d23761a
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2014-07-30 21:25:04 +0100

    Styling changes to IRC & Screenshot pages

--- pages/IRC.php
+++ pages/IRC.php
@@ -1,12 +1,14 @@
 <div class="row clearfix">
     <div class="header"><h1>IRC</h1></div>
     <div class="col double-col-2">
-        <iframe
-	        src="https://kiwiirc.com/client/irc.freenode.net/?nick=SoAer|?&theme=cli#seedofandromeda"
-	        style="border: 0; padding: 0; width: 100%; height: 550px;"></iframe>
+        <div class="text">
+            <iframe
+	            src="https://kiwiirc.com/client/irc.freenode.net/?nick=SoAer|?&theme=cli#seedofandromeda"
+	            style="border: 0; padding: 0; width: 100%; height: 550px;"></iframe>
 
-        <div id="irc-alt-msg">
-	        Kiwi IRC not working? <a href="http://webchat.freenode.net/?channels=seedofandromeda">Try this one!</a>
+            <div id="irc-alt-msg">
+	            Kiwi IRC not working? <a href="http://webchat.freenode.net/?channels=seedofandromeda">Try this one!</a>
+            </div>
         </div>
     </div>
 </div>

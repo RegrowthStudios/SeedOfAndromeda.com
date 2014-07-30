053322310e228bdc21eb689ac4a1a7fa4c33920a
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2014-07-30 20:21:04 +0100

    Added 404 page.

---
+++ pages/404.php
@@ -0,0 +1,8 @@
+<div class="row clearfix">
+    <div class="header"><h1 class="error">Error: 404 - Oh No!</h1></div>
+    <div class="col double-col-2" style="text-align:center;">
+        <div class="text error">
+            Sorry, we couldn't find what you were looking for!
+        </div>
+    </div>
+</div>

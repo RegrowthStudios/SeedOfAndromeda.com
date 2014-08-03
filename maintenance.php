579d28b69cf95c09f2c3c391a4e0519e049beb22
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2014-08-03 17:12:29 +0100

    Static maintenance page

---
+++ maintenance.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>SoA - Maintenance</title>
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/images/favicon.ico" />
+    <link rel="stylesheet" href="/styles/normalise.css" type="text/css" />
+    <link rel="stylesheet" href="/styles/soa.min.css" type="text/css" />
+    <link rel="stylesheet" href="/assets/fonts/the_league_of_orbitron/Orbitron.css" type="text/css" />
+    <link href='http://fonts.googleapis.com/css?family=Electrolize' rel='stylesheet' type='text/css' />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="UTF-8">
+    <meta name="keywords"
+	    content="seed of andromeda, SoA, Windows, PC, Mac, Linux, Voxel, Voxel-Based, Voxel Game, Voxel-Based Game, indie game, independent game, independently developed, video game, pc game, creation, survival, chunks, blocks, sci fi" />
+    <meta name="description"
+	    content="Seed Of Andromeda is a Voxel based Sandbox RPG in a futuristic setting. The player will crash land on a fully round voxel planet and will be forced to survive hostile creatures. As the player progresses through the game, they will be able to form settlements, develop technology, and eventually escape the rock they are stranded on!" />
+    <meta name="og:title" 
+        content="Seed of Andromeda" />
+    <meta name="og:description"
+	    content="Seed Of Andromeda is a Voxel based Sandbox RPG in a futuristic setting. The player will crash land on a fully round voxel planet and will be forced to survive hostile creatures. As the player progresses through the game, they will be able to form settlements, develop technology, and eventually escape the rock they are stranded on!" />
+    <meta name="og:site_name" 
+        content="seedofandromeda" />
+    <meta name="og:type" 
+        content="game" />
+    <meta name="og:url" 
+        content="http://www.seedofandromeda.com/" />
+    <meta name="og:image"
+	    content="http://www.seedofandromeda.com/assets/images/soa_icon.png" />
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js" type="text/javascript"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js" type="text/javascript"></script>
+    <script src="/scripts/lightbox-2.6.js"></script>
+    <link href="/styles/lightbox.css" rel="stylesheet" />
+    <script src="/scripts/soa.js"></script>
+    <script src="/scripts/jquery.sticky.js"></script>
+    <script>
+        $(document).ready(function () {
+            $("#nav-bar").sticky({ topSpacing: 0 });
+        });
+    </script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-46451794-1', 'seedofandromeda.com');
+      ga('send', 'pageview');
+
+    </script>
+</head>
+<body>
+    <div id="main">
+
+        <a href="#">
+            <div id="header"></div>
+        </a>
+
+        <div id="content-wrapper">
+
+            <div id="nav-bar">
+                <ul id="navigation">
+                    <li class="home"><a href="#">Home</a></li>
+                    <li class="about"><a href="#">About</a>
+                        <div class="dropdown-wrapper">
+                            <ul class="dropdown">
+                                <li><a href="#">The Game</a></li>
+                                <li><a href="#">The Team</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="media"><a href="#">Media</a>
+                        <div class="dropdown-wrapper">
+                            <ul class="dropdown">
+                                <li><a href="#">Downloads</a></li>
+                                <li><a href="#">Screenshots</a></li>
+                                <li><a href="#">Videos</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="community"><a href="#">Community</a>
+                        <div class="dropdown-wrapper">
+                            <ul class="dropdown">
+                                <li><a href="#">Forums</a></li>
+                                <li><a href="#">Mods</a></li>
+                                <li><a href="#">IRC</a></li>
+                                <li><a href="http://www.reddit.com/r/seedofandromeda/" target="_blank">Reddit</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="store"><a href="#">Store</a></li>
+                    <li class="blogs"><a href="#">Dev Blogs</a></li>
+                </ul>
+                
+                <div id="account-bar">
+                    <img class="account-image" src="assets/images/DefaultUser_NoSignIn_ProfImg.png" />
+                    <div class="account-overview">
+                        <span class="account-name">Not Logged In</span>
+                    </div>
+                    <div class="account-log-in-wrapper">
+                        <ul class="account-log-in">
+                            <form method='post' action='#'>
+                                <div class="input"> 
+						            <span>Username:</span> <input type='text'
+							            style="padding-right: 0;" id="username" name='login'
+							            placeholder="Username" />
+                                </div>
+                                <div class="input"> 
+						            <span>Password:</span> <input type='password'
+							            style="padding-right: 0;" id='password' name='password'
+							            placeholder="Password" />
+                                </div>
+                                <div class="input">
+                                    <span>Remember Me:</span> <div class="checkbox"> <input id="remember" 
+								        type='checkbox' style="padding-right: 0;" name='remember' />
+							            <label for="remember"></label>
+                                    </div>
+                                </div>
+						        <input type="hidden" name="cookie_check" value="1"> <input
+							        type="hidden" name="redirect" value="#">
+						        <div class="account-log-in-submit"><input type="hidden" name="_xfToken" value="#"> <input
+							        type='submit' value='submit' onClick='prepare_login();'
+							        class="left" /><span class="account-log-in-register">&nbsp;or <a href="#">register</a>.</span></div>
+					        </form>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div id="content-outer">
+                <div class="content-border content-border-0" style="width: 1053px;">
+                    <div class="content-border content-border-1" style="width: 1051px;">
+                        <div class="content-border content-border-2" style="width: 1049px;">
+                            <div class="content-border content-border-3" style="width: 1047px;">
+                                <div class="content-border content-border-2" style="width: 1045px;">
+                                    <div class="content-border content-border-1" style="width: 1043px;">
+                                        <div class="content-border content-border-0" style="width: 1041px;">
+                                        
+                                            <div class="row clearfix">
+                                                <div class="header"><h1 class="error">Maintenance</h1></div>
+                                                <div class="col double-col-2" style="text-align:center;">
+                                                    <div class="text error">
+                                                        This section of the site is currently down for maintenance! We apologise for the inconvenience.
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div id="footer-buffer"></div>
+        <div id="footer">Copyright &copy; 2014 SeedofAndromeda.com</div>
+
+    </div>
+    
+</body>
+</html>

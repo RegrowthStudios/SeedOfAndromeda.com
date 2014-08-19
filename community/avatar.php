fc91bd68952b2643477d9292f6097bb95f4fc7f2
jessenic <jessenic@digiex.net>
2014-08-19 21:42:56 +0300

    Fixed undefined variable error in avatar.php

--- community/avatar.php
+++ community/avatar.php
@@ -7,7 +7,7 @@ require($fileDir . '/library/XenForo/Autoloader.php');
 XenForo_Autoloader::getInstance()->setupAutoloader($fileDir . '/library');
 
 XenForo_Application::initialize($fileDir . '/library', $fileDir);
-XenForo_Application::set('page_start_time', $startTime);
+//XenForo_Application::set('page_start_time', $startTime);
 
 ####################
 

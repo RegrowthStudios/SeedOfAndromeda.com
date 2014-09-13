999967ad79d2e7d7294f3be0b070eea598db2166
TheJeremail <jeremailfischer@aol.com>
2014-09-13 01:22:37 +0100

    Mor styling and demomsg script

--- apps/t3mod.php
+++ apps/t3mod.php
@@ -6,12 +6,12 @@
 //this is just a skeleton style. dont want to mess it up completely :3
 -->
 
-   <!-- <div id="window" style="width:100%; height:550px; position:absolute; top:0px; left:0px;">
+    <div id="window" style="width:100%; height:550px; position:absolute; overflow:hidden; top:0px; left:0px;">
         <div style="height:50px; width:100%; position: absolute; background-color:#dddddd;">
-            Buttons and shit
+            <div>Chan name</div><div>|</div> ([- for status) Buttons
         </div>
-        <div style="height:450px; width:100%; padding:0px; position: absolute; top:50px; background-color:#ffffff;">
-            Other people's shit! This shit is unstyled, because jeremy can't make shit look nice.
+        <div id="msgs" style="height:450px; width:100%; padding:0px; position: absolute; top:50px; background-color:#ffffff;">
+           
         </div>
         <div style="height:50px; width:100%; position: absolute; top:500px; background-color:#eeeeee;">
             <div style="height:50px; width:1px; position: absolute; background-color:#bbbbaa;" id="input">
@@ -25,23 +25,23 @@
             document.getElementById("input").style.width = (window.innerWidth - 80)+"px";
         </script>
     </div>
-    -->
-    <div style="visibility: visible; width:100%;">
+    
+    <div style="visibility: hidden; overflow:hidden; height:0px; width:0px;">
         <!-- Templates for messenger -->
         <div id="mesgin">
-            <div style="width:100%;">
+            <div style="margin:3px;">
                 <div style="width:80%; float:left;">
-                    <div style="background-color:#eeeecc; padding:5px;">
-                        Fuck you!
+                    <div style="background-color:#eeeecc; float:left; padding:5px; min-width:100px; display: inline-block;">
+                        $message.content
                         <div style="width:1px; height:9px;"></div>
-                    </div>
+                    </div><br/><br/>
                     <div style="float:left; width:1px; height:5px; position:relative;">
                         <div style="font-size:9px; position: absolute; top:-14px; left: 5px;">
-                            15:01:11
+                            $message.time.computed
                         </div>
                         <div style="font-size:9px; position: absolute; top:-14px; left: 45px; width:200px;">
                             <div style="float:left;">
-                                Some dickhead just said that.
+                                $message.user
                             </div>
                         </div>
                     </div>
@@ -49,24 +49,56 @@
             </div>
         </div>
         
-        <div id="mesgout" style="width:100%;">
-            <div style="width:100%;">
+        <div id="mesgout" style="">
+            <div style="margin:3px;">
                 <div style="width:80%; float:right;">
-                    <div style="background-color:#eeeecc; padding:5px;">
-                        This swaggy Chat client better be working!<br/> yo
+                    <div style="background-color:#eeeecc; float:right; padding:5px; min-width:100px; display: inline-block;">
+                        $message.content
                         <div style="width:1px; height:9px;"></div>
-                    </div>
+                    </div><br/><br/>
                     <div style="float:right; width:1px; height:5px; position:relative;">
                         <div style="font-size:9px; position: absolute; top:-14px; right: 5px;">
-                            15:02:21
+                            $message.time.computed
                         </div>
                         <div style="font-size:9px; position: absolute; top:-14px; right: 45px; width:200px;">
                             <div style="float:right;">
-                                You just said that, Idiot!
+                                You
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <div id="justNowTimespan">60</div>
+        <div id="messageTimeFormat">$message.time.HH:$message.time.MM:$message.time.SS</div>
+        <div id="ownMessageName">You</div><!-- or $message.user -->
     </div>
+    
+    <script>
+        function computeTime(time){
+            time = new Date(time *1000);
+            return document.getElementById('messageTimeFormat').innerHTML.replace("$message.time.HH",function(){if(time.getHours() < 10){return "0"+time.getHours();}else{return time.getHours();} }).replace("$message.time.MM",function(){if(time.getMinutes() < 10){return "0"+time.getMinutes();}else{return time.getMinutes();} }).replace("$message.time.SS",function(){if(time.getSeconds() < 10){return "0"+time.getSeconds();}else{return time.getSeconds();} });
+        }
+    
+        function msgin(sender,message,time){
+            var template = document.getElementById('mesgin').innerHTML;
+            template = template.replace("$message.user",sender);
+            template = template.replace("$message.time.computed",computeTime(time));
+            template = template.replace("$message.content",message);
+            document.getElementById("msgs").innerHTML+=template;
+        }
+        
+        function msgout(sender,message,time){
+            var template = document.getElementById('mesgout').innerHTML;
+            template = template.replace("$message.user",document.getElementById("ownMessageName").innerHTML);
+            template = template.replace("$message.user",sender);
+            template = template.replace("$message.time.computed",computeTime(time));
+            template = template.replace("$message.content",message);
+            document.getElementById("msgs").innerHTML+=template;
+        }
+    
+        msgin("thejeremail","hi dude",74594356);
+        msgout("DummyUser","swagfsadfasdejsaeffy fyweugdf wegrf qwyegf wqyegf oqywegf qyuwgefqwrigufdsrtgnbh aerugfuqiefieirtuh wadfygaipryg wergiwerjh blah ehfbywrgf aghefiuwagefiugaweg asefrerwegr wer qweygrqyug4r5qyug4rg q45r7g wegr gw rfgq weurgfqwe grwger 9khfgweirgoawurygfger",74594356);
+        msgin("thejeremail","haha, sir spamalot xD",74594356);
+        msgout("DummyUser","hehe, lol",234234534);
+    </script>

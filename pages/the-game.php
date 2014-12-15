f6146e69ad44fc29cde57209c04eab2b5e590227
SamThePsychoticLeprechaun <matthew.marshall96@yahoo.co.uk>
2014-12-15 02:29:24 +0000

    Updated text content of The Game

--- pages/the-game.php
+++ pages/the-game.php
@@ -1,21 +1,52 @@
 <div id="about-the-game" class="row clearfix">
-    <div class="header"><h1>What is Seed of Andromeda?</h1></div>
+    <div class="header"><h1>Your Game</h1></div>
     <div class="col double-col-2">
         <div class="text">
-                Seed of Andromeda is a Voxel based Sandbox RPG in a futuristic setting.
-                The player will crash land on a fully round voxel planet and will be
-                forced to survive hostile creatures. As the player progresses through
-                the game, he will be able to form settlements with NPC survivors. Once
-                the player has progressed enough along the technology path, he will be
-                able to create spaceships so that he may explore the other planets of
-                the solar system, and salvage the wreckage of his fleet in orbit. The
-                game will emphasize customization and moddability, so there will be
-                many tools released with the game including a world editor, tree
-                editor, biome editor, and block editor. The world editor allows players
-                to create full procedural worlds that they may play with and share with
-                others, and the content for these worlds can be created using the other
-                editor tools. SoA is heavily inspired by Minecraft, Starforge,
-                Starmade, Vox, and most other voxel games.
+                The Earth is no longer sufficient for humanity, which has looked to the stars for new homes to expand to. You are amongst the 
+                first wave of colonists to leave Earth for the stars, your destination: the Trinity star system. On your arrival, you are greeted 
+                by a none too friendly, advanced race, who launch a viscous attack on your colonial fleet. In the carnage, you descend in an escape 
+                capsule to the surface of Aldrin, a moon of Hyperion, a gas giant in the Trinity star system. You must make what you can of this new 
+                world, with your limited supplies, what you do is your choice... it is your story that is to unfold.
+                <br/><br/>Seed of Andromeda is your game, you play it, you make your own decisions, and you are the driver of your story. When you play Seed of 
+                Andromeda, you will always create your own story, and every time you play again, you will have an entirely new and unique story to tell! 
+                Seed of Andromeda is a decision-driven game. We want Seed of Andromeda to be about you, the player, not about a pre-written story that 
+                your actions are dictated by.
+            </div>
+    </div>
+</div>
+<div id="about-the-game" class="row clearfix">
+    <div class="header"><h1>Survivalistic</h1></div>
+    <div class="col double-col-2">
+        <div class="text">
+                Now Earth's story is over, and yours is beginning... will you join forces with other survivors and try to create a utopian world? A 
+                dictatorial empire? Maybe go the lone wolf and explore the vast landscapes of the world you have crashed down on? The choices are as 
+                endless as your imagination! The choices you make however, have a great impact on how the world you inhabit evolves; the local tribes 
+                may react badly to your presence on their lands, an advanced race may appreciate your trade with them, all these relationship changes 
+                will affect your further interactions with those groups, so if you want a prosperous relationship, play nice!
+            </div>
+    </div>
+</div>
+<div id="about-the-game" class="row clearfix">
+    <div class="header"><h1>Technologically Focused</h1></div>
+    <div class="col double-col-2">
+        <div class="text">
+                While we stand by the idea of not forcing a story on you, we don't want to leave you with nothing! Seed of Andromeda has a fully-realised 
+                technology tree for you, and other inhabitants of the game's worlds, to progress along. Always wanted that ray gun your mum promised for 
+                your 18th? Well get researching and engineering wondrous technologies! Don't want to while away in a lab? Then get out and explore, find 
+                alien crash sites and learn from the past! Don't much like the idea of trekking through the wilderness? Butter up some NPCs with flattery 
+                and sweet talk and they might divulge some of their hard earned technologies! Or just blow them up and steal it... it's all up to you!
+            </div>
+    </div>
+</div>
+<div id="about-the-game" class="row clearfix">
+    <div class="header"><h1>Full of Space and Round Worlds</h1></div>
+    <div class="col double-col-2">
+        <div class="text">
+                Seed of Andromeda utilises a ground-breaking spherical world system unique to the game, which allows you to generate your own full scale 
+                procedural voxel planets to explore, which not only sounds cool, but is cool! Exploring a world with real horizons, and orbiting from space 
+                has never been more epic. Explore your local solar system and visit other round worlds, explore alien landscapes and meet new sentient 
+                races. Seen all there is to see in your solar system? Explore even further and start to spread your reputation amongst the stars! Seed of 
+                Andromeda makes no exceptions to the unparalleled exploration experience you deserve!
             </div>
     </div>
 </div>

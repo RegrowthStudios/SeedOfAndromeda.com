2ed5f318921401563781a90cb5b6cf83330ef196
SamThePsychoticLeprechaun <matthew.marshall96@yahoo.co.uk>
2014-12-21 00:07:20 +0000

    Updated Auth to Work

--- JereNet/auth.php
+++ JereNet/auth.php
@@ -2,69 +2,77 @@
 require_once ('../community/XenForoSDK.php');
 $sdk = new XenForoSDK ();
 
-$loggedIn = $sdk->isLoggedIn ();
-$userinfo = $sdk->getUser ();
-$visitor = $sdk->getVisitor ();
-
-if(isset($_REQUEST['action'])){
-	switch($_REQUEST['action']){
-		case "checklogin":
-			if(!$loggedIn){
-				die("FAIL\nNot logged in");
-			}else{
-				echo_userdata($userinfo);
-			}
-		break;
-		case "login":
-			if(!isset($_REQUEST['username']) || !isset($_REQUEST['password'])){
-				die("SYNTAX");
-			}else{
-				$login = $sdk->validateLogin($_REQUEST['username'], $_REQUEST['password'], true, true);
-				if(!is_int($login)){
-				die("FAIL");
-				}else{
-				$user = $sdk->getUser ($login);
-				echo_userdata($user);
-				}
-			}
-		break;
-	}
-}else{
-?>
-debug form<br>
-<form action="?action=login" method="post">
-<input type="text" name="username"/>
-<input type="password" name="password"/>
-<input type="submit"/>
-</form>
-<?php
+if ( !isset ( $_POST["u"] ) || !isset ( $_POST["p"] ) ) {
+    echo 0;
+} else {
+    if ( $sdk->validateLogin($_POST["u"], $_POST["p"]) === true ) {
+        $r = echo_userdata();
+        if ($r == -1) {
+            // Fail /w Email
+            echo 0;
+        } else {
+            echo $r;
+        }
+    } else {
+        echo 0;
+    }
 }
 
-function echo_userdata($user){
-echo file_get_contents(
-        //This needs to make the following call s
-        
-        //https://seedofandromeda.com/JereNet/api/?api=net&prot=tosess
-        //Call 1
-        
-        
-        
-        //https://seedofandromeda.com/JereNet/api/?api=net&prot=prtauth
-        // post data:
-        //JerX_Sess= [result from call 1]
-        //prt=SOAC
-        //key=f7fc2a9f7d22138
-        //met=ownu
-        //username=<username>
-
-        'https://seedofandromeda.com/JereNet/api/',
+function echo_userdata(){
+    $sessKey = file_get_contents( 'https://seedofandromeda.com/JereNet/api/?api=net&prot=tosess' );
+    
+    $post = array (
+        "JerX_Sess" => $sessKey,
+        "prt" => "SOAC",
+        "key" => "f7fc2a9f7d22138",
+        "met" => "ownu",
+        "username" => $_POST["u"]
+    );
+    
+    $err = file_get_contents(
+        'https://seedofandromeda.com/JereNet/api/?api=net&prot=prtauth',
         false,
         stream_context_create(
             array( 'http' => array(
                 'method' => 'post',
-                'content' => http_build_query($_POST),
+                'content' => http_build_query($post),
                 ),
             )
         )
     );
+    
+    // E:0 = Username available & logged in
+    // E:1 = API Key Incorrect
+    // E:2 = API Key Incorrect Permissions
+    // E:3 = Username already exists
+    
+    if ($err == "E:0") {
+        return $sessKey;
+    } else if ($err == "E:3") {
+        $post = array (
+            "JerX_Sess" => $sessKey,
+            "prt" => "SOAC",
+            "key" => "f7fc2a9f7d22138",
+            "met" => "ownu",
+            "username" => ( $_POST["u"] . "_SoA" )
+        );
+        $err = file_get_contents(
+            'https://seedofandromeda.com/JereNet/api/?api=net&prot=prtauth',
+            false,
+            stream_context_create(
+                array( 'http' => array(
+                    'method' => 'post',
+                    'content' => http_build_query($post),
+                    ),
+                )
+            )
+        );
+        if ($err == "E:0") {
+            return $sessKey;
+        } else {
+            return -1;
+        }
+    } else {
+        return -1;        
+    }
 }

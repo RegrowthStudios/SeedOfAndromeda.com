a14ff7c8141ccb38084ac2a8af2d256614067d37
SamThePsychoticLeprechaun <matthew.marshall96@yahoo.co.uk>
2015-03-31 01:40:37 +0100

    Updated the text on the game page. Temporary till new design is ready.

--- pages/the-game.php
+++ pages/the-game.php
@@ -2,20 +2,13 @@
     <div class="header"><h1>What is Seed of Andromeda?</h1></div>
     <div class="col double-col-2">
         <div class="text">
-                Seed of Andromeda is a Voxel based Sandbox RPG in a futuristic setting.
-                The player will crash land on a fully round voxel planet and will be
-                forced to survive hostile creatures. As the player progresses through
-                the game, he will be able to form settlements with NPC survivors. Once
-                the player has progressed enough along the technology path, he will be
-                able to create spaceships so that he may explore the other planets of
-                the solar system, and salvage the wreckage of his fleet in orbit. The
-                game will emphasize customization and moddability, so there will be
-                many tools released with the game including a world editor, tree
-                editor, biome editor, and block editor. The world editor allows players
-                to create full procedural worlds that they may play with and share with
-                others, and the content for these worlds can be created using the other
-                editor tools. SoA is heavily inspired by Minecraft, Starforge,
-                Starmade, Vox, and most other voxel games.
+                The Earth is no longer sufficient for humanity, which has looked to the stars for new homes to expand to. You are amongst the first wave of colonists to leave Earth, your destination: 
+                the Trinity star system. On your arrival, you are greeted by a none too friendly, advanced race, who launch a vicious attack on your colonial fleet. In the carnage, you descend in an 
+                escape capsule to the surface of Aldrin, a moon of the gas giant Hyperion. With your limited supplies, you must make what you can of this new world. What you do is your choice… it is 
+                your story that is to unfold.
+                <br/><br/>
+                Earth's story is over, and yours is beginning... will you join forces with other survivors and try to create a utopian world? A dictatorial empire? Maybe go the lone wolf and explore 
+                the vast landscapes of the world you have crashed down on? The choices are as endless as your imagination!
             </div>
     </div>
 </div>
